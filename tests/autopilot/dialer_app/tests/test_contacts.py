7374f6cfe6c04c013d941985d72d8c0bf20e35ed
Omer Akram <om26er@ubuntu.com>
2014-01-23 22:47:45 +0500

    ensure the messaging app opens from call log, still failing due to bug 1271997

--- tests/autopilot/dialer_app/tests/test_contacts.py
+++ tests/autopilot/dialer_app/tests/test_contacts.py
@@ -13,45 +13,72 @@
 from __future__ import absolute_import
 
 from autopilot.matchers import Eventually
+from autopilot.introspection import get_proxy_object_for_existing_process
 from testtools.matchers import Equals
+from ubuntuuitoolkit import emulators as toolkit_emulators
 
 from dialer_app.tests import DialerAppTestCase
 
 import os
 import subprocess
+import time
 
 
 class TestContacts(DialerAppTestCase):
     """Tests for the contacts panel."""
 
-    def setUp(self):
-        # provide clean history
-        self.history = os.path.expanduser(
-            "~/.local/share/history-service/history.sqlite")
-        if os.path.exists(self.history):
-            subprocess.call(["pkill", "history-daemon"])
-            os.rename(self.history, self.history + ".orig")
+    db_file = "history.sqlite"
+    local_db_dir = "dialer_app/data/"
+    system_db_dir = "/usr/lib/python2.7/dist-packages/dialer_app/data/"
 
-        os.environ["HISTORY_SQLITE_DBPATH"] = "dialer_app/data/history.sqlite"
+    if os.path.exists("../../src/dialer-app"):
+        database = local_db_dir + db_file
+    else:
+        database = system_db_dir + db_file
+
+    def setUp(self):
+        subprocess.call(["pkill", "history-daemon"])
+        os.environ["HISTORY_SQLITE_DBPATH"] = self.database
         subprocess.Popen(["history-daemon"], stdout=subprocess.PIPE,
             universal_newlines=True)
 
         super(TestContacts, self).setUp()
 
-    def tearDown(self):
-        super(TestContacts, self).tearDown()
-
-        # restore history
-        if os.path.exists(self.history + ".orig"):
-            subprocess.call(["pkill", "history-daemon"])
-            os.rename(self.history + ".orig", self.history)
-
-    def test_call_log_item_integration(self):
-        """Ensure tapping on different items of a call log opens
-        the right application.
+    def _set_testability_environment_variable(self):
+        """Makes sure every app opened in the current environment loads
+        the testability driver.
 
         """
+        subprocess.call(
+            [
+                "/sbin/initctl",
+                "set-env",
+                "QT_LOAD_TESTABILITY=1"
+            ]
+        )
+
+    def _get_app_pid(self, app):
+        return int(subprocess.check_output(["pidof", app]).strip())
+
+    def test_call_log_item_opens_messaging(self):
+        """Ensure tapping on 'send text message' item of a call log opens
+        the messaging app.
+
+        """
+        self._set_testability_environment_variable()
         self.main_view.switch_to_tab("callLogTab")
         history_item = self.app.select_single(objectName="historyDelegate0")
         self.pointing_device.click_object(history_item)
+        time.sleep(1) # hack right now, needs a "moving" property
+        
         self.assertThat(history_item.detailsShown, Eventually(Equals(True)))
+
+        send_msg_button = self.app.select_single(objectName="logMessageButton")
+        self.pointing_device.click_object(send_msg_button)
+
+        msg_app = get_proxy_object_for_existing_process(
+            self._get_app_pid("messaging-app"),
+            emulator_base=toolkit_emulators.UbuntuUIToolkitEmulatorBase
+            )
+
+        self.assertThat(msg_app.main_view.visible, Eventually(Equals(True)))
