a1cb31f2332c40780d14d36001fb832aa9588b95
Omer Akram <om26er@ubuntu.com>
2014-02-08 18:15:34 +0500

    style improvements, pull out a bit more code into reusable functions

--- tests/autopilot/dialer_app/helpers.py
+++ tests/autopilot/dialer_app/helpers.py
@@ -15,15 +15,50 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from autopilot.platform import model
+
 import subprocess
+import sys
+import time
+
+
+class TelephonyTestHelpers(object):
+
+    def wait_for_incoming_call(self):
+        """Wait up to 5 s for an incoming phone call"""
+
+        timeout = 10
+        while timeout >= 0:
+            out = subprocess.check_output(
+                ['/usr/share/ofono/scripts/list-calls'],
+                stderr=subprocess.PIPE)
+            if 'State = incoming' in out:
+                break
+            timeout -= 1
+            time.sleep(0.5)
+        else:
+            self.fail('timed out waiting for incoming phonesim call')
+
+        # on desktop, notify-osd generates a persistent popup, clean this up
+        if model() == 'Desktop':
+            subprocess.call(['pkill', '-f', 'notify-osd'])
+
+    def invoke_incoming_call(self):
+        """Invoke an incoming call for test purpose."""
+        # magic number 199 will cause a callback from 1234567; dialing 199
+        # itself will fail, so quiesce the error
+        subprocess.call(['/usr/share/ofono/scripts/dial-number', '199'],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 def is_phonesim_running():
     """Determine whether we are running with phonesim."""
     try:
-        out = subprocess.check_output(["/usr/share/ofono/scripts/list-modems"],
-                                        stderr=subprocess.PIPE)
-        return out.startswith("[ /phonesim ]")
+        out = subprocess.check_output(
+            [
+                '/usr/share/ofono/scripts/list-modems',
+            ], stderr=subprocess.PIPE)
+        return out.startswith('[ /phonesim ]'')
     except subprocess.CalledProcessError:
         return False
 
@@ -37,7 +72,10 @@ def ensure_ofono_account():
 
 
 def _is_ofono_account_set():
-    mc_tool = subprocess.Popen(['mc-tool', 'list'], stdout=subprocess.PIPE,
-                                universal_newlines=True)
+    mc_tool = subprocess.Popen(
+        [
+            'mc-tool',
+            'list',
+        ], stdout=subprocess.PIPE, universal_newlines=True)
     mc_accounts = mc_tool.communicate()[0]
     return 'ofono/ofono/account' in mc_accounts
