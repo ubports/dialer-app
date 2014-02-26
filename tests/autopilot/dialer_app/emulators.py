6d23b8c6b4096f2d3ad01076809f2c5469daa805
chris.gagnon <chris.gagnon@canonical.com>
2014-02-26 18:32:39 -0500

    refactor to use page object pattern

--- tests/autopilot/dialer_app/emulators.py
+++ tests/autopilot/dialer_app/emulators.py
@@ -18,24 +18,51 @@ class MainView(toolkit_emulators.MainView):
 
     @property
     def dialer_page(self):
-        return self.select_single(DialerPage)
+        return self.wait_select_single(DialerPage)
+
+    @property
+    def live_call_page(self):
+        return self.wait_select_single(LiveCall)
 
     def get_first_log(self):
         return self.wait_select_single(objectName="historyDelegate0")
 
+    def _click_button(self, button):
+        """Generic way to click a button"""
+        button.visible.wait_for(True)
+        self.pointing_device.click_object(button)
+        return button
 
-class DialerPage(toolkit_emulators.UbuntuUIToolkitEmulatorBase):
+
+class LiveCall(MainView):
+    def __init__(self, *args):
+        super(LiveCall, self).__init__(*args)
+
+    def stop_watch(self):
+        return self.wait_select_single(objectName="stopWatch")
+
+    def _get_hangup_button(self):
+        """Returns the hangup button"""
+        return self.wait_select_single(objectName='hangupButton')
+
+    def click_hangup_button(self):
+        """Clicks and returns the hangup page"""
+        self.visible.wait_for(True)
+        return self._click_button(self._get_hangup_button())
+
+
+class DialerPage(MainView):
     def __init__(self, *args):
         super(DialerPage, self).__init__(*args)
 
-    def get_keypad_entry(self):
+    def _get_keypad_entry(self):
         return self.select_single("KeypadEntry")
 
-    def get_keypad_keys(self):
+    def _get_keypad_keys(self):
         return self.select_many("KeypadButton")
 
-    def get_keypad_key(self, number):
-        buttonsDict = {
+    def _get_keypad_key(self, number):
+        buttons_dict = {
             "0": "buttonZero",
             "1": "buttonOne",
             "2": "buttonTwo",
@@ -50,10 +77,44 @@ class DialerPage(toolkit_emulators.UbuntuUIToolkitEmulatorBase):
             "#": "buttonHash",
         }
         return self.select_single("KeypadButton",
-                                  objectName=buttonsDict[number])
+                                  objectName=buttons_dict[number])
 
-    def get_erase_button(self):
+    def _get_erase_button(self):
+        """Returns the erase button"""
         return self.select_single("CustomButton", objectName="eraseButton")
 
-    def get_call_button(self):
+    def _get_call_button(self):
+        """Returns the call button"""
         return self.select_single(objectName="callButton")
+
+    def click_call_button(self):
+        """Clicks and returns the call button"""
+        return self._click_button(self._get_call_button())
+
+    def click_erase_button(self):
+        """Clicks and returns the erase button"""
+        return self._click_button(self._get_erase_button())
+
+    def click_keypad_button(self, keypad_button):
+        """clicks the keypad button
+
+        :param returns: the clicked keypad_button
+        """
+        return self._click_button(keypad_button)
+
+    def dial_number(self, number):
+        """Dial given number (string) on the keypad and call
+
+        :param number: the number to dial
+        """
+        for digit in number:
+            button = self._get_keypad_key(digit)
+            self.click_keypad_button(button)
+
+        entry = self._get_keypad_entry()
+        entry.value.wait_for(number)
+        return entry
+
+    def call_number(self, number):
+        self.dial_number(number)
+        return self.click_call_button()
