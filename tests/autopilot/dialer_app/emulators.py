4b0da4a280465a26f13f802e1f404185da9b84d3
Leo Arias <leo.arias@canonical.com>
2014-08-01 18:44:36 -0600

    Also sleep before clicking the hangup button.

--- tests/autopilot/dialer_app/emulators.py
+++ tests/autopilot/dialer_app/emulators.py
@@ -52,8 +52,9 @@ class LiveCall(MainView):
     def click_hangup_button(self):
         """Click and return the hangup page"""
         self.visible.wait_for(True)
-        button = self._click_button(self._get_hangup_button())
         import time
+        time.sleep(10)
+        button = self._click_button(self._get_hangup_button())
         time.sleep(10)
         return button
 
