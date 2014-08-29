3c50ad931e1bed496c4074e00d614852b96b420c
Gustavo Pichorim Boiko <gustavo.boiko@canonical.com>
2014-08-29 19:41:15 -0300

    Revert the changes to autopilot. They break tests on mako.

--- tests/autopilot/dialer_app/ListItemWithActions.py
+++ tests/autopilot/dialer_app/ListItemWithActions.py
@@ -27,12 +27,9 @@ class ListItemWithActions(_common.UbuntuUIToolkitCustomProxyObjectBase):
         action_margin = ((self.actionWidth / 5) * 2)
         x_offset = ((self.actionWidth + action_margin) * action_index)
         x_offset += self.actionThreshold
-        # just in case, drag it a bit more to make sure the correct action is
-        # selected
-        x_offset += 2 * action_margin
 
         x, y, width, height = self.globalRect
-        start_x = x + (width * 0.8)
+        start_x = x + (width * 0.5)
         stop_x = start_x - x_offset
         start_y = stop_y = y + (height // 2)
 
